96c9fef4b8f6f442b6be8f3d0af2351d65a833b2
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-22 00:32:08 +0300

    Material UI

---
+++ src/components/TypoButtons.jsx
@@ -0,0 +1,11 @@
+import Typography  from "@mui/material/Typography";
+
+const TypoButtons = () => {
+  return <div>
+  <Typography variant="h1" component="h2">
+    h1. Heading
+  </Typography>;
+  </div>;
+};
+
+export default TypoButtons;
