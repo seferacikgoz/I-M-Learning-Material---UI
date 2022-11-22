971bc8519b534141e6b544b12fb347429fa2cd55
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-22 16:36:01 +0300

    typography

--- src/components/TypoButtons.jsx
+++ src/components/TypoButtons.jsx
@@ -2,9 +2,13 @@ import Typography  from "@mui/material/Typography";
 
 const TypoButtons = () => {
   return <div>
-  <Typography variant="h1" component="h2">
-    h1. Heading
+  <Typography variant="h4" component="h6" align="center" mt={4}>
+    MUI Typography
   </Typography>;
+
+  <Typography variant="body1"  align="justify" mt={4}>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi iusto nesciunt ipsa sapiente. Dicta ipsum, pariatur magni id sunt ut!
+  </Typography>; 
   </div>;
 };
 
