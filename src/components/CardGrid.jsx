b20b0171ed24400ff26e54ea686bf11da6e0dc66
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-23 14:30:18 +0300

    card

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,10 +1,43 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import data from "../data"
 
-const CardGrid = () => {
+export default function CardGrid() {
   return (
-    <div>
-        
-    </div>
-  )
+<>
+    {data.map((card) => {
+        const {id, text, img,name} = card;
+        return(
+            <div>
+        <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={img}
+          alt="img"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          {text}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+        </Card>
+            </div>
+        )
+    })}</>
+    
+  );
 }
-
-export default CardGrid
