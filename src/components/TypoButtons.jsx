1e875d0187a6ae082018d9b3d283626200d0dc61
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-23 12:54:14 +0300

    Container Box Button

--- src/components/TypoButtons.jsx
+++ src/components/TypoButtons.jsx
@@ -1,15 +1,42 @@
 import Typography  from "@mui/material/Typography";
+import Container  from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
 const TypoButtons = () => {
-  return <div>
-  <Typography variant="h4" component="h6" align="center" mt={4}>
-    MUI Typography
-  </Typography>;
-
-  <Typography variant="body1"  align="justify" mt={4}>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi iusto nesciunt ipsa sapiente. Dicta ipsum, pariatur magni id sunt ut!
-  </Typography>; 
-  </div>;
+  return (
+    <>
+    <Container>
+    <Typography 
+    variant="h4" 
+    component="h6"
+    align="center" 
+    mt={4}
+    color="error">
+      MUI Typography
+    </Typography>
+  
+    <Typography variant="body1"  align="justify" mt={4} sx={{background:"lightgreen", 
+      color:"#eee",
+      fontSize:"1.4rem"}}>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi iusto nesciunt ipsa sapiente. Dicta ipsum, pariatur magni id sunt ut!
+    </Typography>
+  
+    <Typography variant="button" sx ={{display: "inline-block", marginTop:"2rem"}}>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque nobis similique rem officia incidunt maiores beatae alias. Cum harum pariatur incidunt totam dolores deleniti delectus ut tempore? Non, architecto id.
+    </Typography>
+    </Container>
+    
+    <Container>
+    <Box sx={{display:"flex", flexDirection:"column", gap:"0.5rem"}}>
+    <Button variant="text">Text</Button>
+    <Button variant="contained">Contained</Button>
+    <Button variant="outlined">Outlined</Button>
+    </Box>
+    </Container>
+    </>
+  
+  )
 };
 
 export default TypoButtons;
